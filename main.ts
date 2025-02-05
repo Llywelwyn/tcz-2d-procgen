78b4f7a2fb42d39259d9e5f477619ac0c74eb4fb
Llywelwyn <82828093+Llywelwyn@users.noreply.github.com>
2025-02-05 10:07:34 +0000

    initial BSP rects

--- main.ts
+++ main.ts
@@ -51,10 +51,31 @@ class Map {
     }
 }
 
+class Rect {
+    constructor(
+        public x1: number,
+        public x2: number,
+        public y1: number,
+        public y2: number
+    ) {}
+
+    public static withSize(x: number, y: number, width: number, height: number): Rect {
+        return new Rect(
+            x, 
+            x + width, 
+            y, 
+            y + height
+        )
+    }
+
+    public getWidth(): number { return (Math.abs(this.x1 - this.x2)); }
+    public getHeight(): number { return (Math.abs(this.y1 - this.y2)); }
+}
+
 /**
  * Static class containing ProcGen logic to be applied to a Map.
  */
-class MapProcGen {
+class MapBuilder {
     /**
      * Counts the living cell neighbours of a given cell in a 2D array of cell states.
      * 
@@ -64,7 +85,7 @@ class MapProcGen {
      * @returns {number} - The number of living (true) neighbours of the cell.
      */
     private static countCellNeighbours(states: boolean[][], row: number, col: number): number {
-        let count = 0;
+        let count: number = 0;
         for (let i = -1; i < 2; i++) {
             for (let j = -1; j < 2; j++) {
                 // Don't count own cell.
@@ -96,14 +117,14 @@ class MapProcGen {
      * @returns {boolean[][]} - 2D array of evolved cell states.
      */
     public static cellularAutomataStep(states: boolean[][]): boolean[][] {
-        const gridRows = states.length;
-        const gridCols = states[0].length;
+        const gridRows: number = states.length;
+        const gridCols: number = states[0].length;
         const newStates: boolean[][] = [];
 
         for (let r = 0; r < gridRows; r++) {
             newStates[r] = [];
             for (let c = 0; c < gridCols; c++) {
-                const livingNeighbours = MapProcGen.countCellNeighbours(states, r, c);
+                const livingNeighbours = MapBuilder.countCellNeighbours(states, r, c);
                 if (
                     (states[r][c] && livingNeighbours >= 4) ||
                     (!states[r][c] && livingNeighbours >= 5 || livingNeighbours === 0)
@@ -115,6 +136,72 @@ class MapProcGen {
             }
         }
         return newStates;
+    }
+
+    public buildBspRects(states: boolean[][]) {
+        const gridRows: number = states.length;
+        const gridCols: number = states[0].length;
+        const rects: Rect[] = [];
+        const rooms: Rect[] = [];
+
+        rects.push(Rect.withSize(2, 2, gridCols - 5, gridRows - 5));
+        const first_room = rects[0];
+        MapBuilder.addSubrects(rects, first_room);
+
+        let n_rooms: number = 0;
+        while (n_rooms < 240) {
+            const rect = MapBuilder.getRandomRect(rects);
+            const candidate = MapBuilder.getRandomSubRect(rect);
+            if (MapBuilder.roomIsPossible(candidate, rooms)) {
+                rooms.push(candidate);
+                MapBuilder.addSubrects(rects, rect);
+            }
+            n_rooms += 1;
+        }
+
+        // TODO: unstatic this stuff and turn it into a proper Builder so rooms
+        //       and rects can be passed around to stuff like a Room or Corridor
+        //       drawer.
+    }
+
+    private static addSubrects(rects: Rect[], rect: Rect) {
+        const width: number = rect.getWidth();
+        const height: number = rect.getHeight();
+        const halfWidth: number = Math.max(width / 2, 1);
+        const halfHeight: number = Math.max(height / 2, 1);
+
+        rects.push(Rect.withSize(rect.x1, rect.y1, halfWidth, halfHeight));
+        rects.push(Rect.withSize(rect.x1, rect.y1 + halfHeight, halfWidth, halfHeight));
+        rects.push(Rect.withSize(rect.x1 + halfWidth, rect.y1, halfWidth, halfHeight));
+        rects.push(Rect.withSize(rect.x1 + halfWidth, rect.y1 + halfHeight, halfWidth, halfHeight));
+    }
+
+    private static getRandomRect(rects: Rect[]): Rect {
+        if (rects.length == 1) { return rects[0]; }
+        return rects[randint(1, rects.length) - 1];
+    }
+
+    private static getRandomSubRect(rect: Rect): Rect {
+        const result = rect;
+        const rectWidth = rect.getWidth();
+        const rectHeight = rect.getHeight();
+        const randomSizeWithinBounds = (min: number, width: number, max: number) => {
+            return Math.max(min, randint(1, Math.min(width, max)) - 1) + 1;
+        }
+
+        let w = randomSizeWithinBounds(3, rectWidth, 10);
+        let h = randomSizeWithinBounds(3, rectHeight, 10);
+
+        result.x1 += randint(1, 6) - 1;
+        result.y1 += randint(1, 6) - 1;
+        result.x2 = result.x1 + w;
+        result.y2 = result.y1 + h;
+
+        return result;
+    }
+
+    private static roomIsPossible(room: Rect, rooms: Rect[]) {
+        return;
     }
 }
 
@@ -163,7 +250,7 @@ const renderCellularAutomataSteps = (count: number) => {
     MapRenderer.drawToImage(map.getStates(), bg.image);
     const drawStep = () => {
         pause(500)
-        map.update(states => MapProcGen.cellularAutomataStep(states));
+        map.update(states => MapBuilder.cellularAutomataStep(states));
         MapRenderer.drawToImage(map.getStates(), bg.image);
     }
     for (let i = 0; i < count; i++) {
